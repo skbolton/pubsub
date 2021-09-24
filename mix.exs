151b87124b67bd9afe98b1f00e76d6f65cfafc1d
Michael St Clair <michaelst57@gmail.com>
2021-09-24 13:12:08 -0600

    setup no-op adapter (#52)
    * setup no-op adapter
    
    * fix dialyzer
    
    * ignore raise in coverage and dialyzer
    
    * change version
    
    * change to logging error instead of raising

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GenesisPubSub.MixProject do
   def project() do
     [
       app: :genesis_pubsub,
-      version: "0.13.2",
+      version: "0.13.3",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
